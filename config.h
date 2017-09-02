35d2b40c7cacd164bad6e54cd1fe0be22f8476ab
Jonathan A. Kollasch <jakllsch@kollasch.net>
2017-09-02 15:49:03 -0500

    entirely remove Firefox rule

--- config.h
+++ config.h
@@ -35,7 +35,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
 	{ "MPlayer",  "xv",       NULL,       0,            1,           -1 },
 };
 
